9d73c98f982810b84c45559e4ac55e4825318d85
Daniel <daniel@miempresa.online>
2026-03-04 22:06:37 +0100

    Fix Vite blank screen: add relative base path ./ to vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Esto asegura que los estáticos se busquen de forma relativa en el hosting
 })
